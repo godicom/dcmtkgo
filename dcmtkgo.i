f8cd0b518d9bd46d38f95429ab126ced57b740d7
antlad <antlad@icloud.com>
2015-02-27 22:41:40 +0300

    Big fun with swig dcmtk go and make

---
+++ dcmtkgo.i
@@ -0,0 +1,6 @@
+%module(directors="1") dcmtkgo 
+%{
+#include "dcmtk/dcmdata/dcitem.h"
+%}
+
+%#include "dcmtk/dcmdata/dcitem.h"
---
+++ dcmtkgo_test.go
@@ -0,0 +1,16 @@
+// #cgo CPPFLAGS: -I/usr/local/include/
+// Copyright 2012 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package dcmtkgo_test
+
+import (
+	"../dcmtkgo"
+	"fmt"
+	"testing"
+)
+
+func TestCall(t *testing.T) {
+	fmt.Println("WTF?!")
+}
