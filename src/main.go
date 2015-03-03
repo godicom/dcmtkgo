cac82d5ed044a1222249715d4fb5e4d6a82c09f0
antlad <antlad@icloud.com>
2015-03-03 04:46:15 +0300

    Rrrrr.. go build system

--- src/main.go
+++ src/main.go
@@ -1,13 +1,16 @@
 package main
 
+//Don't know how to do it yet(may be later with cmake)
+//Add for linux(gcc): -lstdc++
+//Add for mac(clang): -lc++
+
 // #cgo CFLAGS: -I./cppwrap
-// #cgo LDFLAGS: -L./cppwrap -lstdc++ -ldcmtkgo -ldcmdata -lz -lofstd -loflog
+// #cgo LDFLAGS: -L./cppwrap  -ldcmtkgo -ldcmdata -lz -lofstd -loflog
 // #include <export.h>
 import "C"
 import (
 	"fmt"
 )
-//-lc++
 
 func main() {
 	fmt.Printf("Invoking c library...\n")
